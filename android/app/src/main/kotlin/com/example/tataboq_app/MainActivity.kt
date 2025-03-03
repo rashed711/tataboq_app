befe781f630a79ab0cd61b935bbe54e5018f2165
Rashed <rashed1711@gmail.com>
2025-03-03 14:33:27 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/tataboq_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tataboq_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        appBar: AppBar(
+          title: Text('Ahmed Rashed', style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.indigo[800],
+        ),
+
+        body: ImagePage(),
+      ),
+    ),
+  );
+}
+
+class ImagePage extends StatefulWidget {
+  const ImagePage({super.key});
+  @override
+  State<ImagePage> createState() => _ImagePageState();
+}
+
+class _ImagePageState extends State<ImagePage> {
+  int leftImmageNumber = 1;
+  int rightImmageNumber = 2;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Text(
+          leftImmageNumber == rightImmageNumber ? 'مبروك' : 'حاول مرة اخري',
+          style: TextStyle(color: Colors.white, fontSize: 42),
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    leftImmageNumber = Random().nextInt(8) + 1;
+                    rightImmageNumber = Random().nextInt(8) + 1;
+                  });
+                },
+                child: Image.asset('images/image-$leftImmageNumber.png'),
+              ),
+            ),
+
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    rightImmageNumber = Random().nextInt(8) + 1;
+                    leftImmageNumber = Random().nextInt(8) + 1;
+                  });
+                },
+                child: Image.asset('images/image-$rightImmageNumber.png'),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
